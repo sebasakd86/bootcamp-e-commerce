06b90244cea2f8e6ca23607df793d2570a84f3ef
narbaysebas@gmail.com <narbaysebas@gmail.com>
2021-03-02 12:39:38 -0300

    First commit

---
+++ src/home.component.jsx
@@ -0,0 +1,43 @@
+import './home.styles.scss'
+
+const HomePage = () => {
+    return (
+        <div className="homePage">
+            <div className="directory-menu">
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">HATS</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">SHIRTS</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">WOMEN</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">MEN</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">PABLO</h1>
+                        <span className="subtitle">SHOP NOW</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    );
+}
+
+export default HomePage;
