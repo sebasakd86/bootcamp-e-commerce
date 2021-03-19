6d749f4b6f438e1c27630dd8d56124cf8e6776c6
narbaysebas@gmail.com <narbaysebas@gmail.com>
2021-03-19 10:27:19 -0300

    Added collection-overview & directory to styled component fashion (and removed css)

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,18 +1,17 @@
-import { useSelector } from 'react-redux'
-import { selectCollectionsForPreview } from '../../redux/ducks/shopDucks';
-import CollectionPreview from '../collection-preview/collection-preview.component';
-import './collections-overview.styles.scss'
+import { useSelector } from "react-redux";
+import { selectCollectionsForPreview } from "../../redux/ducks/shopDucks";
+import CollectionPreview from "../collection-preview/collection-preview.component";
+import { CollectionsOverviewContainer } from "./collection-overview.styles";
+
 const CollectionsOverview = () => {
-    const collections = useSelector(selectCollectionsForPreview)
+    const collections = useSelector(selectCollectionsForPreview);
     return (
-        <div className="collections-overview">
-            {
-                collections.map(({ id, ...otherProps }) => (
-                    <CollectionPreview key={id} {...otherProps} />
-                ))
-            }
-        </div>
+        <CollectionsOverviewContainer>
+            {collections.map(({ id, ...otherProps }) => (
+                <CollectionPreview key={id} {...otherProps} />
+            ))}
+        </CollectionsOverviewContainer>
     );
-}
- 
+};
+
 export default CollectionsOverview;
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,16 +1,16 @@
 import MenuItem from "../menu-item/menu-item.component";
-import { useSelector } from 'react-redux';
-import { selectDirectorySection } from '../../redux/ducks/directoryDucks'
-import './directory.styles.scss'
+import { useSelector } from "react-redux";
+import { selectDirectorySection } from "../../redux/ducks/directoryDucks";
+import { DirectoryContainer } from "./directory.style";
 
 const Directory = () => {
-  const sections = useSelector(selectDirectorySection);  
-  return (
-    <div className="directory-menu">
-      {
-        sections.map(({ id, ...sectionProps }) => <MenuItem key={id} {...sectionProps} />)
-      }
-    </div>
-  );
-}
+    const sections = useSelector(selectDirectorySection);
+    return (
+        <DirectoryContainer>
+            {sections.map(({ id, ...sectionProps }) => (
+                <MenuItem key={id} {...sectionProps} />
+            ))}
+        </DirectoryContainer>
+    );
+};
 export default Directory;
